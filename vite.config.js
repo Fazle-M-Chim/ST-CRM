dc3f9eb17e33fee7b650df4d72e75148a6b26f46
Fazlemalak Chimthanawala <fazlemalak.asif@gmail.com>
2025-07-17 00:10:26 +0530

    Fix for hosting

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/ST-CRM/', // 👈 critical for GitHub Pages
+  base: '/Sakan-Tradelinks/',
   plugins: [react(), tailwindcss()],
 })
