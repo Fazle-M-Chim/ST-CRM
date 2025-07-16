a174d3fcbe1a8fd1a9db85edfe84eec51d19a6a7
Fazlemalak Chimthanawala <fazlemalak.asif@gmail.com>
2025-07-16 23:35:55 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './', // 👈 critical for GitHub Pages
+  base: '/ST-CRM/', // 👈 critical for GitHub Pages
   plugins: [react(), tailwindcss()],
 })
